1c517e359b34f1ba7aaf598d0f0b5be9a2324cf1
Alvaro Penalva Alberca <alvaro_penalva@hotmail.com>
2025-04-21 14:03:58 +0200

    feature: added new exceptions in controller advice and reformat  response error, and changed username database

--- prices/src/main/java/com/inditex/core/prices/infrastructure/adapters/rest/GlobalExceptionController.java
+++ prices/src/main/java/com/inditex/core/prices/infrastructure/adapters/rest/GlobalExceptionController.java
@@ -1,7 +1,11 @@
 package com.inditex.core.prices.infrastructure.adapters.rest;
 
+import java.util.HashMap;
+import java.util.concurrent.TimeoutException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,14 +15,36 @@ import com.inditex.core.prices.application.exceptions.PriceNotFoundException;
 @ControllerAdvice
 public class GlobalExceptionController {
 
+    private ResponseEntity<HashMap<String, String>> buildResponseEntity(String message, HttpStatus status) {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("error: ", message);
+        response.put("status: ", String.valueOf(status.value()));
+        return new ResponseEntity<>(response, status);
+    }
+
     @ExceptionHandler(PriceNotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(PriceNotFoundException exception){
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<HashMap<String, String>> handleNotFoundException(PriceNotFoundException exception){
+        return buildResponseEntity(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidParamException.class)
-    public ResponseEntity<String> handleInvalidParamException(InvalidParamException exception){
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<HashMap<String, String>> handleInvalidParamException(InvalidParamException exception){
+        return buildResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HashMap<String, String>> handleGeneralException(Exception exception){
+        return buildResponseEntity(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+     @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<HashMap<String, String>> handleHttpMessageNotReadable(MethodArgumentNotValidException ex) {
+        return buildResponseEntity("Invalid param in request", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TimeoutException.class)
+    public ResponseEntity<HashMap<String, String>> handleDatabaseTimeout(Exception ex) {
+        return buildResponseEntity("Database timeout", HttpStatus.GATEWAY_TIMEOUT);
     }
 
 }
